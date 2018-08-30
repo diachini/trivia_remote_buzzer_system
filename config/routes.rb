cc0ae8606c9c566165ba3ef51051c75670a85164
Danny Iachini <danny.iachini@gmail.com>
2018-08-30 19:11:51 -0400

    Updating the route to go straight to page/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'page/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'page/index'
+  root 'page#index'
 end
